3a3c485b73a3efee1f115e649c9cdbde68ebe5f6
ZEROFIVE <65433734+zerofivemyid@users.noreply.github.com>
2024-11-06 11:18:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ import time
 import os
 from colorama import Fore
 
+print("   ____          ____       _                    ")
+print("  | __ )  __ _  |  _ \ __ _| |_ ___ _ __   __ _  ")
+print("  |  _ \ / _' | | |_) / _' | __/ _ \ '_ \ / _' | ")
+print("  | |_) | (_| | |  __/ (_| | ||  __/ | | | (_| | ")
+print("  |____/ \__, | |_|   \__,_|\__\___|_| |_|\__, | ")
+print("         |___/                            |___/  \n")
+print("=================================================")
+author = "Bg.Pateng"
+print("Author: " + author)
+script = "Push Rank Discord"
+print("Script: " + script)
+telegram = "@bangpateng_group"
+print("Telegram: " + telegram)
+youtube = "Bang Pateng"
+print("Youtube: " + youtube)
 print("===========================================")
-print('Thirteen𝕏')
+print('PERINGATAN : TIDAK UNTUK DI PERJUAL-BELIKAN')
 print("===========================================\n")
 
 time.sleep(1)
 
 channel_id = input("Masukkan ID channel: ")
-waktu2 = int(input("Set Waktu Kirim Pesan: "))
+waktu1 = int(input("Set Waktu Kirim Pesan: "))
 
 time.sleep(1)
 print("3")
@@ -52,8 +67,10 @@ while True:
                 is_running = False
                 break
             else:
-              
+                time.sleep(waktu1)
+
+                
         else:
             print(f'Gagal mendapatkan pesan di channel: {response.status_code}')
 
-        time.sleep(waktu2)
+        time.sleep(waktu1)
